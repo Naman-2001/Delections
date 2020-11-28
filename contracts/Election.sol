0986fe19f2d56c63aeada8fce0dc474a0ab8eb08
unknown <naman.aggarwal2001@gmail.com>
2020-11-28 23:21:47 +0530

    front-end

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -8,7 +8,12 @@ contract Election {
     }
 
     mapping(uint256 => Candidate) public candidates;
+    mapping(address => bool) public voters;
     uint256 public candidatesCount;
+
+    event votedEvent(
+        uint indexed _candidateId
+    );
 
     //Constructor
     constructor() public {
@@ -19,5 +24,16 @@ contract Election {
     function addCandidate(string memory _name) private {
         candidatesCount++;
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    function vote(uint _candidateId) public{
+        require(!voters[msg.sender]);
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+        voters[msg.sender] = true;
+
+        candidates[_candidateId].voteCount ++;
+        emit votedEvent(_candidateId);
+
     }
 }
