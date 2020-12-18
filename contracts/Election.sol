121ecb16d5e0426399a1657c4349c55c0a9ee860
unknown <naman.aggarwal2001@gmail.com>
2020-12-19 01:57:42 +0530

    testing contract

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,10 +1,23 @@
 pragma solidity ^0.5.16;
 
 contract Election {
-    string public candidate;
+    struct Candidate {
+        uint256 id;
+        string name;
+        uint256 voteCount;
+    }
+
+    mapping(uint256 => Candidate) public candidates;
+    uint256 public candidatesCount;
 
     //Constructor
     constructor() public {
-        candidate = "Candidate1";
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    function addCandidate(string memory _name) private {
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 }
---
+++ test/election.js
@@ -0,0 +1,32 @@
+var Elections = artifacts.require("./Election.sol");
+
+contract("Election", function (accounts) {
+  it("initializes with two candidates", function () {
+    return Elections.deployed()
+      .then((instance) => {
+        return instance.candidatesCount();
+      })
+      .then((count) => {
+        assert.equal(count, 2);
+      });
+  });
+
+  it("initializes candidates with correct values", () => {
+    return Elections.deployed()
+      .then((instance) => {
+        electionInstance = instance;
+        return electionInstance.candidates(1);
+      })
+      .then((candidate) => {
+        assert.equal(candidate[0], 1, "contains the corret id");
+        assert.equal(candidate[1], "Candidate 1", "contains the corret name");
+        assert.equal(candidate[2], 0, "contains the corret votes count");
+        return electionInstance.candidates(2);
+      })
+      .then((candidate) => {
+        assert.equal(candidate[0], 2, "contains the corret id");
+        assert.equal(candidate[1], "Candidate 2", "contains the corret name");
+        assert.equal(candidate[2], 0, "contains the corret votes count");
+      });
+  });
+});
